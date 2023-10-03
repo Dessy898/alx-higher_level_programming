b1919f077085a11a1da30eeb289880c01c6c8da3
Dessy898 <dessy898@gmail.com>
2023-10-02 17:49:43 -0700

    update all

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bini/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that is design to emphasize code readability"
+        language"
 str = str[39:67] + str[107:112] + str[:6]
 print(str)
